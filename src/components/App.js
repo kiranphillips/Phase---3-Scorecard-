74aba4c6b09b390e28124c91c6a78859aff64622
Lucas Sweeney <lsweeney0124@gmail.com>
2022-07-11 18:29:23 -0400

    updated components, and css styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,13 @@
 import React from "react";
-import Player from "./Player"
-import Hole from ".Hole"
-import Round from "./Round"
-import Login from './Login';
+import Header from './Header';
+import ScoreCard from './ScoreCard'
 
-function App() {
+function App () {
 
   return (
     <div>
-      <NavBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
-      <HeaderMain />
-      <Routes>
-        {/* <Route path="/" element={<TeamPage isLoggedIn={isLoggedIn}/>} /> */}
-        <Route path="/.Player" element={<Player />} />
-        <Route path="/.Hole" element={<Hole />} />
-        <Route path="/.Round" element={<Round />} />
-        <Route path="/.Login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-      </Routes>
-
+      <Header />
+      <ScoreCard />
     </div>
   );
 }
---
+++ src/components/ScoreCard.js
@@ -0,0 +1,14 @@
+import Card from './Card'
+
+function ScoreCard () {
+  return (
+    <div id="scorecard_container">
+      <div>
+        <img id='pin_image' src="https://www.kindpng.com/picc/m/83-839152_golf-flag-golf-clipart-explore-pictures-golf-pin.png" />
+      </div>
+      <Card />
+    </div>
+  )
+}
+
+export default ScoreCard
