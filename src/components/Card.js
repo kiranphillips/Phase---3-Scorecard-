d6c1b3c5aff91f1da632545a9a1a9d0278a3e95d
Lucas Sweeney <lsweeney0124@gmail.com>
2022-07-12 12:19:13 -0400

    changed table to a form, css updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,43 +1,31 @@
 function Card () {
   return (
-    <div>
-      <table>
-        <tr>
-          <th>Hole 1</th>
-          <th>Hole 2</th>
-          <th>Hole 3</th>
-          <th>Hole 4</th>
-          <th>Hole 5</th>
-          <th>Hole 6</th>
-          <th>Hole 7</th>
-          <th>Hole 8</th>
-          <th>Hole 9</th>
-          <th>Hole 10</th>
-          <th>Hole 11</th>
-          <th>Hole 12</th>
-          <th>Hole 13</th>
-          <th>HOLE 14</th>
-          <th>HOLE 15</th>
-          <th>HOLE 16</th>
-          <th>HOLE 17</th>
-          <th>HOLE 18</th>
-        </tr>
-        <tr>
-          <td>Anom</td>
-          <td>19</td>
-          <td>Male</td>
-        </tr>
-        <tr>
-          <td>Megha</td>
-          <td>19</td>
-          <td>Female</td>
-        </tr>
-        <tr>
-          <td>Subham</td>
-          <td>25</td>
-          <td>Male</td>
-        </tr>
-      </table>
+    <div id='score_form'>
+      <form>
+        <label>
+          Strokes:
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+          <input className='data'></input>
+        </label>
+      </form>
+      <button type="submit">Submit Score</button>
     </div>
   )
 }
