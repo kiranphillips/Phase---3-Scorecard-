54a5e2c8e882a9aeef51e142c7545ef2ccc19371
Kiran Phillips <Kiranphillips61@gmail.com>
2022-07-13 17:48:42 -0700

    new components and routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { Routes, Route} from "react-router-dom";
 import Header from './Header';
 import ScoreCard from './ScoreCard'
+import Data from "./Data";
+import Scores from "./Scores";
+
 
 function App () {
 
   return (
     <div>
       <Header />
-      <ScoreCard />
+      {/* <ScoreCard/> */}
+      <Routes> 
+        <Route path="/" element={<ScoreCard />} />
+         <Route path="/Data" element={<Data/>} /> 
+        <Route path="/.Scores" element={<Scores />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Scores.js
@@ -0,0 +1,8 @@
+function Scores (){
+
+<button className='login_button' type="submit">Access All Scores</button>
+
+
+}
+
+export default Scores
