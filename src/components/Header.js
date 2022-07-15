dcdf10eff8ee14ac6f271ff72b4165b9812791b4
Shane Dorsey <scdorsey22@gmail.com>
2022-07-14 20:53:49 -0700

    Underline fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import LogIn from './LogIn';
 function Header () {
   return (
     <div id="header">
-      <LogIn />
       <h1>Whack Fuck!</h1>
     </div>
   )
--- src/components/RouterButton.js
+++ src/components/RouterButton.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 function RouterButton () {
+  const [show, setShow] = useState(false)
+
+
   return (
     <Link to='/rounds'>
-      <button id="router_button">View All Rounds</button>
+      <button onClick={() => setShow(prev => !prev)} id="router_button">View All Rounds</button>
     </Link>
   )
 }
